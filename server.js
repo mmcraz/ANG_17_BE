79a029c35ee68fb64ac274fc4274c61e2ac9cba2
mmcraz <mmcrazhr@gmail.com>
2024-04-06 10:27:54 +0530

    env config change

--- server.js
+++ server.js
@@ -2,11 +2,11 @@
 const mongoose = require('mongoose');
 // const mongoURL = "mongodb+srv://cynnent:cynnent123@cluster0.0ybxpvk.mongodb.net/web";
 const mongoURL = "mongodb+srv://admin:admin@cluster0.lmpvayp.mongodb.net/mmcraz";
-
+require("dotenv").config();
 
 const dbConnection = async () => {
   try {
-    await mongoose.connect(mongoURL); //mm
+    await mongoose.connect(process.env.MONGODB_CONNECT_URI);
     console.log("Database connected successfully");
     return mongoose.connection.db; //This object represents the connected database.
   } catch (error) {
